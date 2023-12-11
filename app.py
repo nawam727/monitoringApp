212519cee08c09cf06b54cf3edf318249c4bb12b
nawam727 <nawamdenawakage@gmail.com>
2023-12-11 22:07:28 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+import psutil
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    cpu_percent = psutil.cpu_percent()
+    mem_percent = psutil.virtual_memory().percent
+    Message = None
+    if cpu_percent > 80 or mem_percent > 80:
+        Message = "High CPU or Memory Utilization detected. Please scale up"
+        
