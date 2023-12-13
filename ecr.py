6f5a12d2ec3ff2002526a5c94033832e7e02e96e
nawam727 <nawamdenawakage@gmail.com>
2023-12-13 14:04:51 +0530

    updated

--- ecr.py
+++ ecr.py
@@ -2,7 +2,7 @@ import boto3
 
 ecr_client = boto3.client('ecr')
 
-repository_name = "monitoringApp"
+repository_name = "monitoringapp"
 response = ecr_client.create_repository(repositoryName=repository_name)
 
 repository_uri = response ['repository']['repositoryUri']
